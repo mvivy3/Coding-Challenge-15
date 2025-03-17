e28a940bcc5d07f0f4fe7393e14e676df97e3e4b
mvivy3 <meredithyen@usf.edu>
2025-03-16 20:02:45 -0400

    Task 1 - Base Structure Setup

---
+++ cc_15.js
@@ -0,0 +1,4 @@
+// TASK 1: CREATING THE BASE STRUCTURE
+const riskDashboard = document.getElementById("riskDashboard"); // selecting the riskDashboard container
+console.log("Risk Dashboard Loaded"); // print message to the console
+
